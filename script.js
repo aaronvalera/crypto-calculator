0de63e822be8bc9265a29919c40f5de3d5353020
Aaron Valera <aaronrvalera@gmail.com>
2026-04-13 15:46:42 -0400

    Crypto market API connection

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ const currencies = document.querySelector("#currency");
 const cryptos = document.querySelector("#cryptocurrency");
 const amount = document.querySelector("#amount");
 
-form.addEventListener("submit", event => {
+form.addEventListener("submit", async event => {
     event.preventDefault();
-    const currencySelected = [...currencies.children].find(currency => currency.selected);
-    const cryptoSelected = [...cryptos.children].find(crypto => crypto.selected);
+    const currencySelected = [...currencies.children].find(currency => currency.selected).value;
+    const cryptoSelected = [...cryptos.children].find(crypto => crypto.selected).value;
     const typedAmount = amount.value;
+try {
+    const response = await (await fetch(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptoSelected}&tsyms=${currencySelected}`)).json();
+    console.log(response.DISPLAY[cryptoSelected][currencySelected].PRICE);
+    console.log(response.DISPLAY[cryptoSelected][currencySelected].HIGH24HOUR);
+    console.log(response.DISPLAY[cryptoSelected][currencySelected].LOW24HOUR);
+    console.log(response.DISPLAY[cryptoSelected][currencySelected].CHANGEPCT24HOUR);
+} catch (error) {
+    console.log(error);
+}
+console.log(cryptoSelected);
 });

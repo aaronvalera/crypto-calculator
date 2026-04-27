13cc8fb9a8858ef6def3541d4aecaa3291c07e60
Aaron Valera <aaronrvalera@gmail.com>
2026-04-26 23:56:15 -0400

    Fix currencies symbols

--- script.js
+++ script.js
@@ -17,16 +17,18 @@ form.addEventListener("submit", async event => {
         const highest24HPrice = response.DISPLAY[cryptoSelected][currencySelected].HIGH24HOUR;
         const lowest24HPrice = response.DISPLAY[cryptoSelected][currencySelected].LOW24HOUR;
         const trend24H = response.DISPLAY[cryptoSelected][currencySelected].CHANGEPCT24HOUR;
+        const trendClass = trend24H >= 0 ? "up-trend" : "down-trend";
+        const trendIcon = trend24H >= 0 ? "▲" : "▼";
+        const currencySymbol = response.DISPLAY[cryptoSelected][currencySelected].TOSYMBOL;
         const quoteResult = amountValue / currentPrice;
         cryptoInfo.innerHTML = `
-            <p class="info price">Price is: <span class="price">$${currentPrice}</span></p>
+            <p class="info price">Price is: <span class="price">${currencySymbol} ${currentPrice}</span></p>
             <p class="info">Highest price is: <span class="price">${highest24HPrice}</span></p>
             <p class="info">Lowest price is: <span class="price">${lowest24HPrice}</span></p>
-            <p class="info">Trend 24H: <span class="price">${trend24H}%</span></p>
-            <p class="info">You can buy: <span class="price">${quoteResult.toFixed(2)}</span></p>
+            <p class="info">Trend 24H: <span class="${trendClass}">${trend24H}%${trendIcon}</span></p>
+            <p class="info">You can buy: <span class="price">${quoteResult.toFixed(2)} ${cryptoSelected}</span></p>
         `;
-        console.log(currentPrice);
     } catch (error) {
     console.log(error);
-    }
+    };
 });
